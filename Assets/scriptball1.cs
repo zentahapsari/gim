d80ad7899c85b574f85229ea72ff09ed2a85ab85
zentahapsari <18523163@students.uii.ac.id>
2020-03-20 14:25:36 +0700

    first commit

---
+++ Assets/scriptball1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptball1 : MonoBehaviour
+{
+    public int speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2 (2,1)*speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.name=="DindingKanan"||other.collider.name=="DindingKiri"){
+            GetComponent<Transform>().position = new Vector2 (0,0);
+        }    
+    }
+    
+}
