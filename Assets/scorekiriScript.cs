77ce474a085d91b162b32db44e5e188f3d9dab3f
zentahapsari <18523163@students.uii.ac.id>
2020-04-11 06:45:05 +0700

    halaman end dan score board

---
+++ Assets/scorekiriScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class scorekiriScript : MonoBehaviour
+{
+    public int counterP1 = 0;
+    public int counterP2 = 0;
+    public Text score1Text;
+    public Text score2Text;
+    public GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score1Text.text = counterP1.ToString();
+        score2Text.text = counterP2.ToString();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+  
+        
+    }
+
+    void player1Score(){
+        counterP1++;
+        score1Text.text = counterP1.ToString();
+    }
+
+    void player2Score(){
+        counterP2++;
+        score2Text.text = counterP2.ToString();
+    }
+
+      
+}
--- Assets/scriptball1.cs
+++ Assets/scriptball1.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class scriptball1 : MonoBehaviour
 {
     public int speed = 30;
+    // public int counter = 0;
+    public int counterP1 = 0;
+    public int counterP2 = 0;
+    public Text scoreText1;
+     public Text scoreText2;
     public Rigidbody2D sesuatu;
     public Animator animtr;
+    
     // Start is called before the first frame update
     void Start()
     { 
+        // scoreText.text = counter.ToString();
         sesuatu.velocity = new Vector2 (-1,-1)*speed;
         animtr.SetBool("IsMove",true);
     }
@@ -25,18 +33,33 @@ public class scriptball1 : MonoBehaviour
     }
 
     void OnCollisionEnter2D(Collision2D other) {
-        if(other.collider.name=="DindingKanan"||other.collider.name=="DindingKiri"){
-            StartCoroutine(jedabola());
+        if(other.collider.name=="DindingKanan"){
+            StartCoroutine(jedabolaP1());
             // GetComponent<Transform>().position = new Vector2 (0,0);
-            
+        } if (other.collider.name=="DindingKiri"){
+            StartCoroutine(jedabolaP2());
         }
     }
 
     // perintah
-    IEnumerator jedabola() {
+    IEnumerator jedabolaP1() {
     sesuatu.velocity = Vector2.zero;
     animtr.SetBool("IsMove",false);
     sesuatu.GetComponent<Transform>().position = Vector2.zero;
+    counterP1++;
+    scoreText1.text = counterP1.ToString();
+    yield return new WaitForSeconds(3);
+    sesuatu.velocity = new Vector2 (-1,-1)*speed;
+    animtr.SetBool("IsMove",true);
+    
+    }
+
+    IEnumerator jedabolaP2() {
+    sesuatu.velocity = Vector2.zero;
+    animtr.SetBool("IsMove",false);
+    sesuatu.GetComponent<Transform>().position = Vector2.zero;
+    counterP2++;
+    scoreText2.text = counterP2.ToString();
     yield return new WaitForSeconds(3);
     sesuatu.velocity = new Vector2 (-1,-1)*speed;
     animtr.SetBool("IsMove",true);
